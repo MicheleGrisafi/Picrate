0cf8f2b924bec07d36b53bc10ec50461c572bca4
MicheleGrisafi <michele.grisafi@studenti.unitn.it>
2017-04-28 14:24:25 +0200

    Progetto iniziale

---
+++ app/src/main/java/androidlab/fotografando/MainActivity.java
@@ -0,0 +1,73 @@
+package androidlab.fotografando;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.Button;
+import android.hardware.camera2.*;
+import android.content.Context;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost); //Tabhost = tab manager
+        tabHost.setup();    //Inizializzo
+
+        //Tab 1
+        TabHost.TabSpec spec = tabHost.newTabSpec("Challenges Tab");    //Una spec è una componente del tabhost, la creo con un identificatore
+        spec.setContent(R.id.tabChallenges);    //Setto la mia tab. Volendo potrei impostare una nuova INTENT invece di una tab
+        spec.setIndicator("", getResources().getDrawable(R.drawable.ic_check_box_black_24dp)); //Dichiaro l'icona
+        tabHost.addTab(spec);   //aggiungo la spec al mio host
+        //Tab 1
+        spec = tabHost.newTabSpec("Rating Tab");
+        spec.setContent(R.id.tabRating);
+        spec.setIndicator("", getResources().getDrawable(R.drawable.ic_star_black_24dp));
+        tabHost.addTab(spec);
+        //Tab 1
+        spec = tabHost.newTabSpec("Ranking Tab");
+        spec.setContent(R.id.tabRanking);
+        spec.setIndicator("", getResources().getDrawable(R.drawable.ic_star_black_24dp));
+        tabHost.addTab(spec);
+
+
+        final Intent takePhotoIntent = new Intent(this,takePhoto.class);
+        Button btn = (Button) findViewById(R.id.btn1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(takePhotoIntent, 200);
+            }
+        });
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode==1) {
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+}
